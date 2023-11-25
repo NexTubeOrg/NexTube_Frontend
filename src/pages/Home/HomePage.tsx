4cf9adb4de16bc3c4b82b9a76dc86fc8730293f3
Denus Berest <denysberesttest@gmail.com>
2023-11-25 09:17:14 +0200

    block processing button on loading

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,10 +1,7 @@
-import VideoCommentsLoader from '../../components/Comments/CommentsContainer/VideoCommentsLoader';
-
 const HomePage = () => {
   return (
     <>
       <h1>this is Home page</h1>
-      <VideoCommentsLoader videoId={3}></VideoCommentsLoader>
     </>
   );
 };
