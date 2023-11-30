5094d010d8f76a8d86263197f7a72d5e0831a4df
Devloner <tminecraftben@gmail.com>
2023-11-30 21:22:04 +0200

    user can react to video

--- src/components/Reactions/AddVideoReaction.tsx
+++ src/components/Reactions/AddVideoReaction.tsx
@@ -2,15 +2,21 @@ import { HandThumbUpIcon, HandThumbDownIcon } from '@heroicons/react/20/solid';
 import { DoubleIconedProcessingButton } from '../common/buttons/DoubleIconedButton';
 import { useEffect, useState } from 'react';
 import http_api from '../../services/http_api';
-import { IVideoReactions } from './types';
+import { IVideoReactions, ReactionTypes } from './types';
 import numeral from 'numeral';
+import { useSelector } from 'react-redux';
+import { IAuthUser } from '../../store/reducers/auth/types';
+import { handleError } from '../../common/handleError';
 
 export const AddVideoReaction = (props: { videoId: number }) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [reactions, setReactions] = useState<IVideoReactions>({
     likes: 0,
     dislikes: 0,
+    wasLikedByRequester: false,
+    wasDislikedByRequester: false,
   });
+  const { isAuth } = useSelector((store: any) => store.auth as IAuthUser);
 
   useEffect(() => {
     const loadReactions = async () => {
@@ -18,7 +24,9 @@ export const AddVideoReaction = (props: { videoId: number }) => {
 
       const result = (
         await http_api.get<IVideoReactions>(
-          `/api/Video/Reaction/GetCountVideoReactions/${props.videoId}`,
+          `/api/Video/Reaction/GetCountVideoReactions${
+            isAuth ? 'WithStatus' : ''
+          }/${props.videoId}`,
         )
       ).data;
       setReactions(() => result);
@@ -28,18 +36,62 @@ export const AddVideoReaction = (props: { videoId: number }) => {
 
     loadReactions();
   }, [props.videoId]);
+
+  const likeVideo = async () => {
+    if (!isAuth) {
+      handleError('Sign in to like');
+      return;
+    }
+    setReactions((r) => {
+      return {
+        likes: r.wasLikedByRequester ? r.likes - 1 : r.likes + 1,
+        dislikes: r.wasDislikedByRequester ? r.dislikes - 1 : r.dislikes,
+        wasLikedByRequester: !r.wasLikedByRequester,
+        wasDislikedByRequester: false,
+      };
+    });
+
+    const request = {
+      videoId: props.videoId,
+      reactionType: ReactionTypes.Like,
+    };
+
+    await http_api.post<any>(`/api/Video/Reaction/ReactVideo`, request);
+  };
+
+  const dislikeVideo = async () => {
+    if (!isAuth) {
+      handleError('Sign in to dislike');
+      return;
+    }
+    setReactions((r) => {
+      return {
+        likes: r.wasLikedByRequester ? r.likes - 1 : r.likes,
+        dislikes: r.wasDislikedByRequester ? r.dislikes - 1 : r.dislikes + 1,
+        wasLikedByRequester: false,
+        wasDislikedByRequester: !r.wasDislikedByRequester,
+      };
+    });
+
+    const request = {
+      videoId: props.videoId,
+      reactionType: ReactionTypes.Dislike,
+    };
+    await http_api.post<any>(`/api/Video/Reaction/ReactVideo`, request);
+  };
+
   return (
     <>
       <div className="flex mr-5">
         <DoubleIconedProcessingButton
           isLoadingLeft={isLoading}
-          onClickLeft={() => {}}
+          onClickLeft={() => likeVideo()}
           textLeft={`${numeral(reactions.likes).format('0a').toUpperCase()}`}
           typeLeft="button"
           backgroundClassnameLeft="primary"
           iconLeft={<HandThumbUpIcon></HandThumbUpIcon>}
           isLoadingRight={isLoading}
-          onClickRight={() => {}}
+          onClickRight={() => dislikeVideo()}
           textRight={`${
             reactions.dislikes > 0
               ? numeral(reactions.dislikes).format('0a').toUpperCase()
@@ -47,6 +99,8 @@ export const AddVideoReaction = (props: { videoId: number }) => {
           }`}
           typeRight="button"
           iconRight={<HandThumbDownIcon></HandThumbDownIcon>}
+          iconLeftColor={reactions.wasLikedByRequester ? 'success' : 'white'}
+          iconRightColor={reactions.wasDislikedByRequester ? 'danger' : 'white'}
           backgroundClassnameRight="secondary"
         ></DoubleIconedProcessingButton>
       </div>
