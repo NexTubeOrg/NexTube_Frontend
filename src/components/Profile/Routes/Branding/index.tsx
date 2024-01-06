b3dda00e1e0d08b465a0da58b867a4cb45b77055
QusDev <qusdeveloper@gmail.com>
2024-01-06 11:06:44 +0200

    add change banner

--- src/components/Profile/Routes/Branding/index.tsx
+++ src/components/Profile/Routes/Branding/index.tsx
@@ -1,18 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { PrimaryProcessingButton } from '../../../common/buttons/PrimaryProcessingButton';
 import { ModalCropper } from '../../../ModalCropper';
-import { useSelector } from 'react-redux';
-import { IAuthUser } from '../../../../store/reducers/auth/types';
 import http_api from '../../../../services/http_api';
+import { handleError, handleSuccess } from '../../../../common/handleError';
+
+interface IchannelPhoto {
+  ChannelPhotoFile: File | null;
+}
+
+interface IChangeBannerRequest {
+  Banner: File | null;
+}
 
 export const ProfileBranding = () => { 
-   interface IchannelPhoto{
-    ChannelPhotoFile: File | null;
-}
-  
+
   const [userData, setUserData] = useState<IchannelPhoto>({
-    ChannelPhotoFile:null,
+    ChannelPhotoFile: null,
   });
+  const [banner, setBanner] = useState<File | null>(null);
+
   const onImageSaveHandler = (file: File) => {
     console.log('image save handle', file);
     setUserData((prevData) => ({
@@ -20,29 +26,48 @@ export const ProfileBranding = () => {
       ChannelPhotoFile: file,
     }));
   };
-   
+
   const handleUploadButtonClick = async () => {
     try {
       console.log("lox");
-     
-  
-        await http_api.put('/api/User/UpdateChannelImage', userData, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-         
-          },
-        });   
-           window.location.reload();
-       
+      
+      await http_api.put('/api/User/UpdateChannelImage', userData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+
+        },
+      });
+      window.location.reload();
+
     } catch (error) {
       console.error('Error saving changes:', error);
     }
   };
-  
 
+  const onBannerSaveHandler = (file: File) => {
+    setBanner(file);
+  };
 
+  const handleUploadBannerClick = async () => {
+    try {
 
-  
+      const changeBannerRequest: IChangeBannerRequest = {
+        Banner: banner
+      };
+
+      await http_api.put('/api/User/changeBanner', changeBannerRequest, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      handleSuccess("Banner successfully uploaded.")
+
+    } catch (error) {
+      console.error('Error saving changes:', error);
+      handleError("Banner upload failed.");
+    }
+  };
 
   return (
     <>
@@ -51,11 +76,11 @@ export const ProfileBranding = () => {
         <div className="flex">
           <div className="left">
             <div className="w-36 h-36 dark:bg-primary rounded-full mr-6">
-            <ModalCropper
-          onSave={onImageSaveHandler}
-          error={''}
-     
-        ></ModalCropper>
+              <ModalCropper
+                onSave={onImageSaveHandler}
+                error={''}
+
+              ></ModalCropper>
             </div>
           </div>
           <div className="right w-80">
@@ -70,7 +95,7 @@ export const ProfileBranding = () => {
                 text="Upload"
                 type="button"
               ></PrimaryProcessingButton>
-               
+
             </div>
           </div>
         </div>
@@ -79,12 +104,16 @@ export const ProfileBranding = () => {
         <h3 className="text-white text-xl mb-2 font-bold">Banner image</h3>
         <div className="flex">
           <div className="left">
-            <div className="mr-6">
-              <img
+            <div className="mr-6 w-36 fill-white dark:bg-transparent">
+              {/* <img
                 className="w-36 fill-white dark:bg-transparent"
                 src="/public/thumb_banner.png"
                 alt=""
-              />
+              /> */}
+              <ModalCropper
+                onSave={onBannerSaveHandler}
+                error={''}
+              ></ModalCropper>
             </div>
           </div>
           <div className="right w-80">
@@ -94,7 +123,7 @@ export const ProfileBranding = () => {
             </p>
             <div className="w-35 mt-6">
               <PrimaryProcessingButton
-                onClick={() => {}}
+                onClick={handleUploadBannerClick}
                 isLoading={false}
                 text="Upload"
                 type="button"
@@ -122,7 +151,7 @@ export const ProfileBranding = () => {
             </p>
             <div className="w-35 mt-6">
               <PrimaryProcessingButton
-                onClick={() => {}}
+                onClick={() => { }}
                 isLoading={false}
                 text="Upload"
                 type="button"
