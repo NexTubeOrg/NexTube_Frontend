8c9a2cde48bf9ca5def04f353794850c64b0b46a
alextereshyt <alextereshyt@gmail.com>
2024-01-26 10:16:49 +0100

    localization fix

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,12 @@ export default defineConfig({
     proxy: {
       // Proxy /api requests to your API server
       '/api': {
-        target: 'https://localhost:7192', // Replace with your API server's URL
+        target: 'https://bnext.itstep.click/', // Replace with your API server's URL
         changeOrigin: true,
         secure: false,
       },
       '/signalr': {
-        target: 'https://localhost:7192',
+        target: 'https://bnext.itstep.click/',
         changeOrigin: true,
         secure: false,
         ws: true, // it`s important to use SignalR with Websockets as transport
